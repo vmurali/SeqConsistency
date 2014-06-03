1010374cb86db90373cb2a6ed69d957c9125809f
Muralidaran Vijayaraghavan <vmurali@csail.mit.edu>
2014-06-02 20:25:06 -0400

    added atomic reg which might prove useful

--- StoreAtomicity.v
+++ StoreAtomicity.v
@@ -3,50 +3,50 @@ Require Import DataTypes.
 Set Implicit Arguments.
 
 Section StoreAtomicity.
-  Record ReqA := { descA: Desc;
-                   dataAQ: Data
+  Record Req := { desc: Desc;
+                  dataQ: Data
+                }.
+
+  Record Resp := { procR: Proc;
+                   idx: Index;
+                   datum: Data
                  }.
 
-  Record RespA := { procAR: Proc;
-                    idxA: Index;
-                    datumA: Data
-                  }.
+  Variable reqFn: Addr -> Proc -> Index -> Req.
 
-  Parameter reqFnA: Addr -> Proc -> Index -> ReqA.
-
-  Variable respFnA: Addr -> Time -> option RespA.
+  Variable respFn: Addr -> Time -> option Resp.
 
   Variable a: Addr.
 
   Definition noStore t :=
-    match respFnA a t with
-      | Some (Build_RespA c' i' d') =>
-          descA (reqFnA a c' i') = St -> False
+    match respFn a t with
+      | Some (Build_Resp c' i' d') =>
+          desc (reqFn a c' i') = St -> False
       | _ => True
     end.
 
   Record StoreAtomicity :=
     {
       uniqRespLabels:
-        forall t1 t2, match respFnA a t1, respFnA a t2 with
-                        | Some (Build_RespA c1 i1 _), Some (Build_RespA c2 i2 _) =>
+        forall t1 t2, match respFn a t1, respFn a t2 with
+                        | Some (Build_Resp c1 i1 _), Some (Build_Resp c2 i2 _) =>
                           c1 = c2 -> i1 = i2 -> t1 = t2
                         | _, _ => True
                       end;
 
       localOrdering:
-        forall t1 t2, match respFnA a t1, respFnA a t2 with
-                        | Some (Build_RespA c1 i1 _), Some (Build_RespA c2 i2 _) =>
+        forall t1 t2, match respFn a t1, respFn a t2 with
+                        | Some (Build_Resp c1 i1 _), Some (Build_Resp c2 i2 _) =>
                           c1 = c2 -> i1 > i2 -> t1 > t2
                         | _, _ => True
                       end;
 
       allPrevious:
-        forall t2, match respFnA a t2 with
-                     | Some (Build_RespA c2 i2 _) =>
+        forall t2, match respFn a t2 with
+                     | Some (Build_Resp c2 i2 _) =>
                        forall i1, i1 < i2 -> exists t1, t1 < t2 /\
-                                                        match respFnA a t1 with
-                                                          | Some (Build_RespA c1 i _) =>
+                                                        match respFn a t1 with
+                                                          | Some (Build_Resp c1 i _) =>
                                                             c1 = c2 /\ i = i1
                                                           | None => False
                                                         end
@@ -55,17 +55,17 @@ Section StoreAtomicity.
 
       storeAtomicity:
         forall t,
-          match respFnA a t with
-            | Some (Build_RespA c i d) =>
-              let (descQ, dtQ) := reqFnA a c i in
+          match respFn a t with
+            | Some (Build_Resp c i d) =>
+              let (descQ, dtQ) := reqFn a c i in
               match descQ with
                 | Ld =>
                   (d = initData a /\ forall t', t' < t -> noStore t') \/
                   (exists tm,
                      tm < t /\
-                     match respFnA a tm with
-                       | Some (Build_RespA cm im dm) =>
-                         let (descQm, dtQm) := reqFnA a cm im in
+                     match respFn a tm with
+                       | Some (Build_Resp cm im dm) =>
+                         let (descQm, dtQm) := reqFn a cm im in
                          d = dtQm /\ descQm = St /\
                          forall t', tm < t' < t -> noStore t'
                        | _ => False
