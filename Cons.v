21b8bec86746f1a281282c800b2d2018739f872a
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2014-06-09 02:46:08 -0400

    Store atomicity cnos

--- Cons.v
+++ Cons.v
@@ -6,192 +6,232 @@ Set Implicit Arguments.
 Definition Tag := nat.
 
 Inductive Req :=
-| LdCommitReq: Addr -> Req
-| LdReq: Tag -> Addr -> Req
-| StReq: Addr -> Data -> Req.
+| LdCommitReq: Req
+| LdReq: Tag -> Req
+| StReq: Data -> Req.
 
 Inductive Resp :=
 | LdCommitResp: Data -> Resp
 | LdResp: Tag -> Data -> Resp
 | StResp: Resp.
 
-Inductive Inst :=
-| Nm: Inst
-| Ld: Addr -> Inst
-| St: Addr -> Data -> Inst.
+Variable Pc: Set.
+Variable State: Set.
+Variable DeltaState: Set.
+
+Inductive HElem :=
+| Nm: DeltaState -> HElem
+| Ld: Addr -> HElem
+| St: Addr -> Data -> HElem.
+
+Inductive HistElem :=
+| Nmh: DeltaState -> HistElem
+| Ldh: Addr -> Data -> DeltaState -> HistElem
+| Sth: Addr -> Data -> HistElem.
+
+Definition Hist := list (Pc * HistElem).
+
+Inductive TransType := Internal | External.
 
 Section PerProc.
-  Variable Pc: Set.
-  Variable State: Set.
-  Variable DeltaState: Set.
-
-  Variable nextSt: Pc -> option Data -> (Pc * Inst * DeltaState).
   Variable updSt: State -> DeltaState -> State.
+  Variable getHElem: Pc -> State -> (Pc * HElem).
+  Variable getLoadDelta: Pc -> State -> Data -> DeltaState.
 
-  Inductive Normal: State -> Pc -> list Req -> list Resp -> bool ->
-                    State -> Pc -> list Req -> list Resp -> bool -> Prop :=
-  | ComNonMem: forall s nextPc p2m m2p nextPc' delS,
-                 nextSt nextPc None = (nextPc', Nm, delS) ->
-                 Normal s nextPc p2m m2p false (updSt s delS) nextPc' p2m m2p false
-  | ComSt: forall s nextPc p2m m2p nextPc' delS a v,
-             nextSt nextPc None = (nextPc', St a v, delS) ->
-             Normal s nextPc p2m m2p false (updSt s delS) nextPc' (StReq a v :: p2m) m2p true
-  | ComStRp: forall s nextPc p2m m2p,
-               Normal s nextPc p2m (StResp :: m2p) true s nextPc p2m m2p false
-  | ComLd: forall s nextPc p2m m2p nextPc' delS a,
-             nextSt nextPc None = (nextPc', Ld a, delS) ->
-             Normal s nextPc p2m m2p false s nextPc (LdCommitReq a :: p2m) m2p true
-  | ComLdRp:
-      forall s nextPc p2m m2p nextPc' delS a v,
-        nextSt nextPc (Some v) = (nextPc', Ld a, delS) ->
-        Normal s nextPc p2m (LdCommitResp v :: m2p) true (updSt s delS) nextPc' p2m m2p false
-  | ExternalReq:
-      forall s nextPc p2m m2p x w,
-        Normal s nextPc (p2m ++ (x :: nil)) m2p w s nextPc p2m m2p w
-  | ExternalResp:
-      forall s nextPc p2m m2p x w,
-        Normal s nextPc p2m (m2p ++ (x :: nil)) w s nextPc p2m m2p w.
+  Variable Rob Ppc: Set.
+  Variable retire compute empty : Rob -> Rob.
+  Variable add: Rob -> Pc -> Rob.
+  Variable getLd: Rob -> option (Tag * Addr).
+  Variable issueLd: Rob -> Tag -> Rob.
+  Variable updLd: Rob -> Tag -> Data -> Rob.
+  Variable getComPc: Rob -> option Pc.
+  Variable commit: Rob -> option (Pc * Pc * HistElem).
+  Variable next: Ppc -> Ppc.
+  Variable set: Ppc -> Pc -> Ppc.
+  Variable get: Ppc -> Pc.
 
-  Variable Rob: Set.
+  Definition updQ A qs idx (v: A) idx' :=
+    if decAddr idx idx'
+    then  v :: qs idx'
+    else qs idx'.
 
-  Definition CommitEntry := (Pc * Pc * Inst * option Data * DeltaState)%type.
-
-  Variable enq: Pc -> Rob -> Rob.
-
-  Variable Ppc: Set.
-  Variable nextPpc: Ppc -> Ppc.
-  Variable execute: Rob -> Rob.
-  Variable getLoad: Rob -> option (Tag * Addr)%type.
-  Variable issueLoad: Tag -> Rob -> Rob.
-  Variable updLoad: Tag -> Data -> Rob -> Rob.
-  Variable getCommitPc: Rob -> option Pc.
-  Variable empty: Rob.
-  Variable setPpc: Pc -> Ppc.
-  Variable getCommitFull: Rob -> option CommitEntry.
-  Variable finishCommit: Rob -> Rob.
-
-  Inductive CommitState :=
-  | StCom: CommitState
-  | LdCom: Pc -> Addr -> Data -> DeltaState -> CommitState.
+  Definition rmQ A (qs: Addr -> list A) idx idx' :=
+    if decAddr idx idx'
+    then tail (qs idx')
+    else qs idx'.
 
   Inductive Spec:
-    State -> Pc -> list Req -> list Resp -> Rob ->
-    option (CommitState) -> Ppc ->
-    State -> Pc -> list Req -> list Resp -> Rob ->
-    option (CommitState) -> Ppc -> Set :=
-  | SpecNotLd: forall s nextPc p2m m2p rob nextCommit ppc,
-                 Spec s nextPc p2m m2p rob nextCommit ppc
-                      s nextPc p2m m2p (enq nextPc rob) nextCommit (nextPpc ppc)
-  | SpecExecute: forall s nextPc p2m m2p rob nextCommit ppc,
-                   Spec s nextPc p2m m2p rob nextCommit ppc
-                        s nextPc p2m m2p (execute rob) nextCommit ppc
-  | SpecLdReq:
-      forall s nextPc p2m m2p rob nextCommit ppc tag a,
-        getLoad rob = Some (tag, a) ->
-        Spec s nextPc p2m m2p rob nextCommit ppc
-             s nextPc (LdReq tag a :: p2m) m2p (issueLoad tag rob) nextCommit ppc
-  | SpecLdResp:
-      forall s nextPc p2m m2p rob nextCommit ppc tag v,
-        Spec s nextPc p2m (LdResp tag v :: m2p) rob nextCommit ppc
-             s nextPc p2m m2p (updLoad tag v rob) nextCommit ppc
+    Hist -> State -> Pc -> (Addr -> list Req) -> list Resp -> bool -> Rob -> Ppc ->
+    Hist -> State -> Pc -> (Addr -> list Req) -> list Resp -> bool -> Rob -> Ppc ->
+    TransType -> Prop :=
+  | SpecFetch:
+      forall h st pc p2m m2p w rob ppc,
+        Spec h st pc p2m m2p w rob ppc
+             h st pc p2m m2p w (add rob (get ppc)) (next ppc)
+             Internal
+  | SpecExec:
+      forall h st pc p2m m2p w rob ppc,
+        Spec h st pc p2m m2p w rob ppc
+             h st pc p2m m2p w (compute rob) ppc
+             Internal
+  | SpecLdRq:
+      forall h st pc p2m m2p w rob ppc tag a,
+        getLd rob = Some (tag, a) ->
+        Spec h st pc p2m m2p w rob ppc
+             h st pc (updQ p2m a (LdReq tag)) m2p w (issueLd rob tag) ppc
+             Internal
+  | SpecLdRp:
+      forall h st pc p2m m2p w rob ppc tag v xs,
+        m2p = LdResp tag v :: xs ->
+        Spec h st pc p2m m2p w rob ppc
+             h st pc p2m xs w (updLd rob tag v) ppc
+             Internal
   | SpecAbort:
-      forall s nextPc p2m m2p rob ppc,
-        getCommitPc rob <> Some nextPc ->
-        Spec s nextPc p2m m2p rob None ppc
-             s nextPc p2m m2p empty None (setPpc nextPc)
+      forall h st pc p2m m2p rob ppc pc',
+        getComPc rob = Some pc' -> pc' <> pc ->
+        Spec h st pc p2m m2p false rob ppc
+             h st pc p2m m2p false (empty rob) (set ppc pc)
+             Internal
   | SpecComNm:
-      forall s nextPc p2m m2p rob ppc delS nextPc',
-        getCommitFull rob = Some (nextPc, nextPc', Nm, None, delS) ->
-        nextSt nextPc None = (nextPc', Nm, delS) ->
-        Spec s nextPc p2m m2p rob None ppc
-             (updSt s delS) nextPc' p2m m2p (finishCommit rob) None ppc
-  | SpecComStReq:
-      forall s nextPc p2m m2p rob ppc delS nextPc' a v,
-        getCommitFull rob = Some (nextPc, nextPc', St a v, None, delS) ->
-        nextSt nextPc None = (nextPc', St a v, delS) ->
-        Spec s nextPc p2m m2p rob None ppc
-             (updSt s delS) nextPc' (StReq a v :: p2m) m2p
-             (finishCommit rob) (Some StCom) ppc
-  | SpecComStResp:
-      forall s nextPc p2m m2p rob ppc,
-        Spec s nextPc p2m (StResp :: m2p) rob (Some StCom) ppc
-             s nextPc p2m m2p (finishCommit rob) None ppc
-  | SpecComLdReq:
-      forall s nextPc p2m m2p rob ppc delS nextPc' a v,
-        getCommitFull rob = Some (nextPc, nextPc', Ld a, Some v, delS) ->
-        Spec s nextPc p2m m2p rob None ppc
-             s nextPc (StReq a v :: p2m) m2p
-             (finishCommit rob) (Some (LdCom nextPc' a v delS)) ppc
-  | SpecComLdRespGood:
-      forall s nextPc p2m m2p rob ppc delS nextPc' a v,
-        nextSt nextPc (Some v) = (nextPc', Ld a, delS) ->
-        Spec s nextPc p2m (LdCommitResp v :: m2p) rob (Some (LdCom nextPc' a v delS)) ppc
-             (updSt s delS) nextPc' p2m m2p (finishCommit rob) None ppc
-  | SpecComLdRespBad:
-      forall s nextPc p2m m2p rob ppc delS nextPc' a v v' delS',
-        nextSt nextPc (Some v') = (nextPc', Ld a, delS') ->
-        Spec s nextPc p2m (LdCommitResp v' :: m2p) rob (Some (LdCom nextPc' a v delS)) ppc
-             (updSt s delS') nextPc' p2m m2p (finishCommit rob) None ppc
-  | SpecExternalReq:
-      forall s nextPc p2m m2p x rob nextCommit ppc,
-        Spec s nextPc (p2m ++ (x :: nil)) m2p rob nextCommit ppc
-             s nextPc p2m m2p rob nextCommit ppc
-  | SpecExternalResp:
-      forall s nextPc p2m m2p x rob nextCommit ppc,
-        Spec s nextPc p2m (m2p ++ (x :: nil)) rob nextCommit ppc
-             s nextPc p2m m2p rob nextCommit ppc.
+      forall h st pc p2m m2p rob ppc nextPc delS,
+        commit rob = Some (pc, nextPc, Nmh delS) ->
+        getHElem pc st = (nextPc, Nm delS) ->
+        Spec h st pc p2m m2p false rob ppc
+             ((pc, Nmh delS) :: h) (updSt st delS) nextPc p2m m2p false rob ppc
+             Internal
+  | SpecComStRq:
+      forall h st pc p2m m2p rob ppc nextPc a v,
+        commit rob = Some (pc, nextPc, Sth a v) ->
+        getHElem pc st = (nextPc, St a v) ->
+        Spec h st pc p2m m2p false rob ppc
+             h st pc (updQ p2m a (StReq v)) m2p true rob ppc
+             Internal
+  | SpecComStRp:
+      forall h st pc p2m m2p rob ppc nextPc a v xs,
+        commit rob = Some (pc, nextPc, Sth a v) ->
+        getHElem pc st = (nextPc, St a v) ->
+        m2p = StResp :: xs ->
+        Spec h st pc p2m m2p false rob ppc
+             ((pc, Sth a v) :: h) st nextPc p2m xs false rob ppc
+             Internal
+  | SpecComLdRq:
+      forall h st pc p2m m2p rob ppc nextPc a v delS,
+        commit rob = Some (pc, nextPc, Ldh a v delS) ->
+        getHElem pc st = (nextPc, Ld a) ->
+        Spec h st pc p2m m2p false rob ppc
+             h st pc (updQ p2m a LdCommitReq) m2p true rob ppc
+             Internal
+  | SpecComLdRpGood:
+      forall h st pc p2m m2p rob ppc nextPc a v delS xs,
+        commit rob = Some (pc, nextPc, Ldh a v delS) ->
+        getHElem pc st = (nextPc, Ld a) ->
+        m2p = LdCommitResp v :: xs ->
+        getLoadDelta pc st v = delS ->
+        Spec h st pc p2m m2p false rob ppc
+             ((pc, Ldh a v delS) :: h) (updSt st delS) nextPc p2m xs false rob ppc
+             Internal
+  | SpecComLdRpBad:
+      forall h st pc p2m m2p rob ppc nextPc a v delS xs v' delS',
+        commit rob = Some (pc, nextPc, Ldh a v delS) ->
+        getHElem pc st = (nextPc, Ld a) ->
+        m2p = LdCommitResp v' :: xs ->
+        v <> v' ->
+        getLoadDelta pc st v' = delS' ->
+        Spec h st pc p2m m2p false rob ppc
+             ((pc, Ldh a v' delS') :: h) (updSt st delS') nextPc p2m xs false
+             (empty rob) (set ppc nextPc)
+             Internal
+  | SpecMemDeqs:
+      forall h st pc p2m m2p w rob ppc p2m' m2p',
+        Spec h st pc p2m m2p w rob ppc
+             h st pc p2m' m2p' w rob ppc
+             External.
 
-  Section SpecList.
-    Variable s0: State.
-    Variable n0: Pc.
-    Variable ppc0: Ppc.
-    Inductive SpecList:
-      State -> Pc -> list Req -> list Resp -> Rob -> option CommitState -> Ppc ->
-      (Addr -> Data) -> option Resp -> Prop :=
-    | InitSpec: SpecList s0 n0 nil nil empty None ppc0 initData None
-    | LdSpec: forall s nextPc p2m m2p rob cs ppc t a m,
-                Spec s nextPc (p2m ++ (LdReq t a :: nil)) m2p rob cs ppc
-                     s nextPc p2m m2p rob cs ppc ->
-                SpecList s nextPc (p2m ++ (LdReq t a :: nil)) m2p rob cs ppc m None ->
-                SpecList s nextPc p2m m2p rob cs ppc m (Some (LdResp t (m a)))
-    | LdCommitSpec: forall s nextPc p2m m2p rob cs ppc a m,
-                      Spec s nextPc (p2m ++ (LdCommitReq a :: nil)) m2p rob cs ppc
-                           s nextPc p2m m2p rob cs ppc ->
-                      SpecList s nextPc (p2m ++ (LdCommitReq a :: nil)) m2p rob cs ppc m None ->
-                      SpecList s nextPc p2m m2p rob cs ppc m (Some (LdCommitResp (m a))).
-    | LdSpec: forall s nextPc p2m m2p rob cs ppc t a m,
-                Spec s nextPc (p2m ++ (LdReq t a :: nil)) m2p rob cs ppc
-                     s nextPc p2m m2p rob cs ppc ->
-                SpecList s nextPc (p2m ++ (LdReq t a :: nil)) m2p rob cs ppc m None ->
-                SpecList s nextPc p2m m2p rob cs ppc m (Some (LdResp t (m a)))
-    | LdCommitSpec: 
-                          
-    | CreateSpec:
-        forall s nextPc p2m m2p rob cs ppc s' nextPc' p2m' m2p' rob' cs' ppc',
-          Spec s nextPc p2m m2p rob cs ppc s' nextPc' p2m' m2p' rob' cs' ppc' ->
-          SpecList s nextPc p2m m2p rob cs ppc ->
-          SpecList s' nextPc' p2m' m2p' rob' cs' ppc'.
+  Definition Mem := Addr -> Data.
 
-    Inductive NormalList:
-      State -> Pc -> list Req -> list Resp -> bool -> Prop :=
-    | InitNormal: NormalList s0 n0 nil nil false
-    | CreateNormal: forall s nextPc p2m m2p w s' nextPc' p2m' m2p' w',
-                      Normal s nextPc p2m m2p w s' nextPc' p2m' m2p' w' ->
-                      NormalList s nextPc p2m m2p w ->
-                      NormalList s' nextPc' p2m' m2p' w'.
+  Definition updP A st p (v: A) p' :=
+    if decProc p p'
+    then v
+    else st p'.
 
-  Theorem specNormal:
-    forall s nextPc p2m m2p rob cs ppc,
-      SpecList s nextPc p2m m2p rob cs ppc ->
-      exists p2m' m2p',
-        NormalList s nextPc p2m' m2p' (match cs with
-                                         | Some _ => true
-                                         | None => false
-                                       end).
-  Proof.
-    admit.
-  Qed.
+  Definition updM A mem a (v: A) a' :=
+    if decAddr a a'
+    then v
+    else mem a'.
 
-  End SpecList.
+  Record ProcState :=
+    { hist: Hist;
+      getPc: Pc;
+      state: State
+    }.
 
+  Inductive CorrectSystem: (Proc -> ProcState) -> Mem ->
+                           (Proc -> ProcState) -> Mem -> Set :=
+  | Load:
+      forall p st m a nextPc delS,
+        getHElem (getPc (st p)) (state (st p)) = (nextPc, Ld a) ->
+        getLoadDelta (getPc (st p)) (state (st p)) (m a) = delS ->
+        CorrectSystem
+          st m
+          (updP st p (Build_ProcState ((getPc (st p), Ldh a (m a) delS) :: hist (st p))
+                                      nextPc (updSt (state (st p)) delS))) m
+  | Store:
+      forall p st m a nextPc v,
+        getHElem (getPc (st p)) (state (st p)) = (nextPc, St a v) ->
+        CorrectSystem
+          st m
+          (updP st p (Build_ProcState ((getPc (st p), Sth a v) :: hist (st p))
+                                      nextPc (state (st p)))) (updM m a v)
+  | NonMem:
+      forall p st m nextPc delS,
+        getHElem (getPc (st p)) (state (st p)) = (nextPc, Nm delS) ->
+        CorrectSystem
+          st m
+          (updP st p (Build_ProcState ((getPc (st p), Nmh delS) :: hist (st p))
+                                      nextPc (updSt (state (st p)) delS))) m.
+
+  Variable initPc: Pc.
+  Variable initState: State.
+  Variable initRob: Rob.
+  Variable initPpc: Ppc.
+
+  Record SpecState :=
+    { hist': Hist;
+      st: State;
+      pc: Pc;
+      p2m: Addr -> list Req;
+      m2p: list Resp;
+      wait: bool;
+      rob: Rob;
+      ppc: Ppc
+    }.
+
+  CoInductive SpecFinal: (Proc -> SpecState) -> Set :=
+  | Int:
+      forall state p h st pc p2m m2p wait rob ppc
+             h' st' pc' p2m' m2p' wait' rob' ppc',
+        state p = Build_SpecState h st pc p2m m2p wait rob ppc ->
+        Spec h st pc p2m m2p wait rob ppc
+             h' st' pc' p2m' m2p' wait' rob' ppc' Internal ->
+        SpecFinal (updP state p (Build_SpecState h' st' pc' p2m' m2p' wait' rob' ppc')) ->
+        SpecFinal state
+  | Ext:
+      forall state p h st pc p2m m2p wait rob ppc
+             h' st' pc' p2m' m2p' wait' rob' ppc' a x y,
+        state p = Build_SpecState h st pc p2m m2p wait rob ppc ->
+        Spec h st pc p2m m2p wait rob ppc
+             h' st' pc' p2m' m2p' wait' rob' ppc' External ->
+        p2m a = p2m' a ++ (x :: nil) ->
+        (forall a', a' <> a -> p2m a = p2m' a) ->
+        m2p' = m2p ++ (y :: nil) ->
+        SpecFinal (updP state p (Build_SpecState h' st' pc' p2m' m2p' wait' rob' ppc')) ->
+        SpecFinal state.
+
+  About Int.
+
+  CoFixpoint getReq sta :=
+    match sta with
+      | Ext _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ a x y _ _ p2m1 p2m2 m2prel future => None
+      | Int _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ future => None
+    end.
 End PerProc.
--- StoreAtomicity.v
+++ StoreAtomicity.v
@@ -1,52 +1,52 @@
 Require Import DataTypes.
 
 Set Implicit Arguments.
+
 Record Req := { desc: Desc;
                 dataQ: Data
               }.
 
-Record Resp := { procR: Proc;
+Record Resp := { addrR: Addr;
+                 procR: Cache;
                  idx: Index;
                  datum: Data
                }.
 
 Section StoreAtomicity.
-  Variable reqFn: Addr -> Proc -> Index -> Req.
+  Variable reqFn: Addr -> Cache -> Index -> Req.
 
-  Variable respFn: Addr -> Time -> option Resp.
+  Variable respFn: Time -> option Resp.
 
-  Variable a: Addr.
-
-  Definition noStore t :=
-    match respFn a t with
-      | Some (Build_Resp c' i' d') =>
-          desc (reqFn a c' i') = St -> False
+  Definition noStore a t :=
+    match respFn t with
+      | Some (Build_Resp a' c' i' d') =>
+          a' = a -> desc (reqFn a c' i') = St -> False
       | _ => True
     end.
 
   Record StoreAtomicity :=
     {
       uniqRespLabels:
-        forall t1 t2, match respFn a t1, respFn a t2 with
-                        | Some (Build_Resp c1 i1 _), Some (Build_Resp c2 i2 _) =>
-                          c1 = c2 -> i1 = i2 -> t1 = t2
+        forall t1 t2, match respFn t1, respFn t2 with
+                        | Some (Build_Resp a1 c1 i1 _), Some (Build_Resp a2 c2 i2 _) =>
+                          a1 = a2 -> c1 = c2 -> i1 = i2 -> t1 = t2
                         | _, _ => True
                       end;
 
       localOrdering:
-        forall t1 t2, match respFn a t1, respFn a t2 with
-                        | Some (Build_Resp c1 i1 _), Some (Build_Resp c2 i2 _) =>
-                          c1 = c2 -> i1 > i2 -> t1 > t2
+        forall t1 t2, match respFn t1, respFn t2 with
+                        | Some (Build_Resp a1 c1 i1 _), Some (Build_Resp a2 c2 i2 _) =>
+                          a1 = a2 -> c1 = c2 -> i1 > i2 -> t1 > t2
                         | _, _ => True
                       end;
 
       allPrevious:
-        forall t2, match respFn a t2 with
-                     | Some (Build_Resp c2 i2 _) =>
+        forall t2, match respFn t2 with
+                     | Some (Build_Resp a2 c2 i2 _) =>
                        forall i1, i1 < i2 -> exists t1, t1 < t2 /\
-                                                        match respFn a t1 with
-                                                          | Some (Build_Resp c1 i _) =>
-                                                            c1 = c2 /\ i = i1
+                                                        match respFn t1 with
+                                                          | Some (Build_Resp a1 c1 i _) =>
+                                                            a1 = a2 /\ c1 = c2 /\ i = i1
                                                           | None => False
                                                         end
                      | _ => True
@@ -54,19 +54,19 @@ Section StoreAtomicity.
 
       storeAtomicity:
         forall t,
-          match respFn a t with
-            | Some (Build_Resp c i d) =>
+          match respFn t with
+            | Some (Build_Resp a c i d) =>
               let (descQ, dtQ) := reqFn a c i in
               match descQ with
                 | Ld =>
-                  (d = initData a /\ forall t', t' < t -> noStore t') \/
+                  (d = initData a /\ forall t', t' < t -> noStore a t') \/
                   (exists tm,
                      tm < t /\
-                     match respFn a tm with
-                       | Some (Build_Resp cm im dm) =>
-                         let (descQm, dtQm) := reqFn a cm im in
-                         d = dtQm /\ descQm = St /\
-                         forall t', tm < t' < t -> noStore t'
+                     match respFn tm with
+                       | Some (Build_Resp am cm im dm) =>
+                         let (descQm, dtQm) := reqFn am cm im in
+                         a = am /\ d = dtQm /\ descQm = St /\
+                         forall t', tm < t' < t -> noStore a t'
                        | _ => False
                      end)
                 | St => d = initData zero 
