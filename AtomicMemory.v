2e05f310533a4974229b78818a4fba16d36361fd
Muralidaran Vijayaraghavan <vmurali@csail.mit.edu>
2014-06-17 22:15:25 -0400

    stuff

--- AtomicMemory.v
+++ AtomicMemory.v
@@ -1,16 +1,27 @@
 Require Import StoreAtomicity DataTypes.
 
+Inductive AtomicMemory : (Addr -> Data) -> (Addr -> Data) -> Set :=
+| AReq m a (w: Desc) d: AtomicMemory m
+                                     (if w
+                                      then m
+                                      else fun a' => if decAddr a a'
+                                                     then d
+                                                     else m a')
+| Idle m: AtomicMemory m m.
+
+Definition getAtomicMemoryInfo m m' (am: AtomicMemory m m') :=
+  match am with
+    | AReq _ a w d => Some (a, w, d, if w
+                                     then m a
+                                     else initData zero)
+    | Idle _ => None
+  end.
+
 Section ReqResp.
   Variable reqFn: Addr -> Proc -> Index -> Req.
   Variable respFn: Time -> option Resp.
 
-  Inductive AtomicMemory : (Addr -> Proc) -> (Addr -> Proc) -> Set :=
-  | AReq: forall m a (w: Desc) d, AtomicMemory m
-                                               (if w
-                                                then m
-                                                else fun a' => if decAddr a a'
-                                                               then d
-                                                               else m a')
-  | Idle: forall m, AtomicMemory m m.
+  Variable sa: StoreAtomicity reqFn respFn.
 
   
+End ReqResp.
